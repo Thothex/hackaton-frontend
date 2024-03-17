29a9bd37730a5b8895d1de5886c1d5aae293a058
Evgeny Leventsov <leventsovee@answer-42.ru>
2024-03-17 23:01:28 +0300

    checkbox to login form

--- src/components/CAuthButton/index.jsx
+++ src/components/CAuthButton/index.jsx
@@ -1,11 +1,17 @@
 import './styles.module.scss';
+import PropTypes from 'prop-types';
+
 const AuthButton = (props) => {
     const { buttonText } = props;
 
     return (
-        <button>
+        <button type='submit'>
             {buttonText}
         </button>
     );
+};
+
+AuthButton.propTypes = {
+    buttonText: PropTypes.string.isRequired
 };
 export default AuthButton;
--- src/components/pages/LoginPage/index.jsx
+++ src/components/pages/LoginPage/index.jsx
@@ -6,8 +6,10 @@ import Checkbox from "@/components/CCheckbox/index.jsx";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
 import axios from "axios";
+import CCheckbox from '../../CustomCheckbox/index.jsx';
 const LoginPage = () => {
     const navigate = useNavigate();
+    const [rememberCheckbox, setRememberCheckbox] = useState(false);
     const [formData, setFormData] = useState({
         username: '',
         password: ''
@@ -34,6 +36,10 @@ const LoginPage = () => {
     const handleNavigate = (path) => {
         navigate(path);
     };
+
+    const handleChangeRemember = (checked) => {
+        setRememberCheckbox(checked)
+    }
     return(
         <div className={styles.main}>
             <img src={logo} alt='logo'/>
@@ -44,8 +50,7 @@ const LoginPage = () => {
                 <AuthInput label='Password' inner='Enter your password' type='password' name='password' value={formData.password} onChange={handleInputChange} />
                 <div>
                     <div>
-                        <Checkbox id="box" />
-                        <label htmlFor="box">Remember me</label>
+                        <CCheckbox label="Remember me" checked={rememberCheckbox} onChange={handleChangeRemember} />
                     </div>
                     <span>Forgot password</span>
                 </div>
