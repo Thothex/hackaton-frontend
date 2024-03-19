585beade2f2fa9d3c9de5f2ede6da4726af63d22
Evgeny Leventsov <leventsovee@answer-42.ru>
2024-03-19 13:51:49 +0300

    dropDown component

---
+++ src/components/CDropDown/index.jsx
@@ -0,0 +1,62 @@
+import styles from "./styles.module.scss";
+import arrowSVG from "../../assets/ddArrow.svg";
+import { useState } from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+
+const CDropDown = ({ items = [], placeholder='Выберите', value='Choose', onChange }) => {
+	
+	console.log(items);
+	const [isOpen, setIsOpen] = useState(false);
+	const handleClick = () => {
+		setIsOpen(!isOpen);
+	}
+	const dpopedPlainClass = classnames(styles.dpopedPlain, {
+		[styles.opened]: isOpen,
+	});
+		
+	const ddArrowClass = classnames(styles.arrowSVG, {
+		[styles.opened]: isOpen,
+	});
+
+
+
+	const handleItemClick = (item) => {
+		setIsOpen(false);
+		onChange(item);
+	}
+	return (
+	<div className={styles.ddContainer}>
+			<label className={styles.label}>{placeholder}</label>
+			<div className={styles.ddWrapper} onClick={handleClick}>
+				<div className={styles.innerWrapper}>
+					<div className={styles.ddText}>{value}</div>
+					<div className={ddArrowClass}>
+						<img className={ddArrowClass} src={arrowSVG} alt="arrow" />
+					</div>
+				</div>
+		</div>
+			<div className={dpopedPlainClass}>
+				{items.map((item) => {
+					return (
+						<div
+							key={item.id}
+							className={styles.ddItem}
+							onClick={()=>handleItemClick(item)}>
+								{item.value} 
+						</div>
+					);
+				})}
+		</div>
+	</div>
+	)
+};
+
+CDropDown.propTypes = {
+	items: PropTypes.array.isRequired,
+	placeholder: PropTypes.string,
+	itemPlaceholder: PropTypes.string,
+	onChange: PropTypes.func.isRequired,
+};
+
+export default CDropDown;
