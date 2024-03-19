518ba20e310da53d5faf664a8200ebd96682e625
test <alinaluzanova@gmail.com>
2024-03-19 16:09:54 +0300

    start page

--- src/App.jsx
+++ src/App.jsx
@@ -11,27 +11,29 @@ import Navbar from "./components/CNavbar";
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserThunk } from './redux/features/userSlice';
+import StartPage from "@/components/pages/StartPage/index.jsx";
 
 function App() {
     const location = useLocation();
   const dispatch = useDispatch()
   const { bearer: bearerFromStore } = useSelector((state) => state.userStore)
-  
+
   useEffect(() => {
       const bearer = localStorage.getItem('token')
       if (bearer) {
         dispatch(getUserThunk())
       }
   }, [dispatch, bearerFromStore])
-  
+
 
   return (
     <div className="appContainer">
-      {location.pathname !== "/register" && location.pathname !== "/login" && (
+      {location.pathname !== "/register" && location.pathname !== "/login"  && location.pathname !== "/" && (
         <Navbar />
       )}
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path='/' element={<StartPage/>} />
+        <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/hackathon" element={<HackathonPage />} />
