f2d29030e1eca683e00929657c741833aee257e0
test <alinaluzanova@gmail.com>
2024-03-26 15:27:27 +0300

    fix bug

--- src/components/pages/StartHackathonPage/index.jsx
+++ src/components/pages/StartHackathonPage/index.jsx
@@ -1,28 +1,30 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchHackathonById } from "@/redux/features/hackathonsSlice.js";
 import styles from "./style.module.scss";
 import {
-  createTeam,
-  getTeamInfo,
-  sendInvite,
+    createTeam,
+    getTeamInfo,
+    sendInvite,
 } from "@/redux/features/teamSlice.js";
 import { getAllUsersThunk } from "@/redux/features/userSlice.js";
 import InvintationBlock from './InvintationBlock';
+
 const StartHackathonPage = () => {
     const navigate = useNavigate();
     const { id } = useParams();
     const dispatch = useDispatch();
     const hackathon = useSelector(state => state.hackathons.hackathon);
     const {teamInfo} = useSelector(state => state.team);
+    const { allUsers, userInfo: user } = useSelector(state => state.userStore);
     const [teamName, setTeamName] = useState('');
     const [inviteEmail, setInviteEmail] = useState('');
     const [searchTerm, setSearchTerm] = useState('');
-    const { allUsers, userInfo: user } = useSelector(state => state.userStore);
     const [newTeamId, setNewTeamId] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+
     useEffect(() => {
         setLoading(true);
         dispatch(fetchHackathonById(id));
@@ -39,8 +41,7 @@ const StartHackathonPage = () => {
 
     useEffect(() => {
         const socket = new WebSocket('ws://localhost:3000');
-        socket.onopen = () => {
-        };
+        socket.onopen = () => {};
         socket.onmessage = (event) => {
             const teamMessage = JSON.parse(event.data);
             if (+teamMessage.teamId === teamInfo.team.id) {
@@ -48,35 +49,34 @@ const StartHackathonPage = () => {
             }
         };
         socket.onclose = () => {
-          console.log('Соединение закрыто');
+            console.log('Connection closed');
         };
         socket.onerror = (error) => {
-          console.error('Ошибка:', error);
+            console.error('Error:', error);
         };
         return () => {
-          socket.close();
+            socket.close();
         };
-      }, [dispatch, id, teamInfo?.team.id, user]);
+    }, [dispatch, id, teamInfo?.team.id, user]);
 
-  const filteredUsers = allUsers
-    ? allUsers.filter(
-        (user) =>
-          user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          user.username.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    : [];
+    const filteredUsers = allUsers
+        ? allUsers.filter(
+            (user) =>
+                user.email.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                user.username.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        : [];
 
-  const handleInputChange = (e) => {
-    const { value } = e.target;
-    setInviteEmail(value);
-    setSearchTerm(value.trim());
-  };
+    const handleInputChange = (e) => {
+        const { value } = e.target;
+        setInviteEmail(value);
+        setSearchTerm(value.trim());
+    };
 
-  const handleUserClick = (email) => {
-    setInviteEmail(email);
-    setSearchTerm("");
-  };
-
+    const handleUserClick = (email) => {
+        setInviteEmail(email);
+        setSearchTerm("");
+    };
 
     const handleCreateTeam = async () => {
         try {
@@ -106,63 +106,36 @@ const StartHackathonPage = () => {
     if (!hackathon || loading) {
         return <div>Loading...</div>;
     }
-  };
 
-  const handleSendInvite = async () => {
-    try {
-      await dispatch(
-        sendInvite({
-          teamId: team.team.id,
-          member: inviteEmail,
-          hackathonId: id,
-        })
-      );
-      console.log("newTeamId:", newTeamId);
-    } catch (error) {
-      console.error("Failed to send invite:", error);
+    if (error) {
+        return <div>Error: {error}</div>;
     }
-  };
 
-  const handleTasksClick = () => {
-    navigate(`/hackathon/${id}/tasks`);
-  };
+    const currentDate = new Date();
+    const startDate = new Date(hackathon.start);
 
-  if (!hackathon || loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  const currentDate = new Date();
-  const startDate = new Date(hackathon.start);
-
-  if (currentDate < startDate) {
+    if (currentDate < startDate) {
+        return (
+            <div>
+                Hackathon has not started yet. Please wait until it starts on {startDate.toDateString()}.
+            </div>
+        );
+    }
 
     return (
-      <div>
-        Hackathon has not started yet. Please wait until it starts in{" "}
-        {startDate.getDate()}.
-      </div>
-    );
-  }
-
-  return (
-    <div style={{ margin: "20px" }}>
-      <div className={styles.hackathonHeader}>
-        <div className={styles.hackathonInfo}>
-          <h1>{hackathon.name}</h1>
-          <div className={styles.Info}>
-            <h3>{hackathon.description}</h3>
-            <h4>{hackathon.rules}</h4>
-          </div>
-        </div>
-
-            <div className={styles.team}>
-                {teamInfo?.team ? (<h2>Your team is: {teamInfo.team.name}</h2>)
-                :
-                    (
+        <div style={{ margin: "20px" }}>
+            <div className={styles.hackathonHeader}>
+                <div className={styles.hackathonInfo}>
+                    <h1>{hackathon.name}</h1>
+                    <div className={styles.Info}>
+                        <h3>{hackathon.description}</h3>
+                        <h4>{hackathon.rules}</h4>
+                    </div>
+                </div>
+                <div className={styles.team}>
+                    {teamInfo?.team ? (
+                        <h2>Your team is: {teamInfo.team.name}</h2>
+                    ) : (
                         <>
                             <h2>Gather your team!</h2>
                             <form onSubmit={handleCreateTeam}>
@@ -170,21 +143,20 @@ const StartHackathonPage = () => {
                                 <button type="submit">Save</button>
                             </form>
                         </>
-                    )
-                }
-                {teamInfo?.teamUsers.length > 0 && (
-                    <InvintationBlock
-                        styles={styles}
-                        teamInfo={teamInfo}
-                        handleSendInvite={handleSendInvite}
-                        handleInputChange={handleInputChange}
-                        inviteEmail={inviteEmail}
-                        searchTerm={searchTerm}
-                        filteredUsers={filteredUsers}
-                        handleUserClick={handleUserClick}
-                    
-                    />
-                )}
+                    )}
+                    {teamInfo?.teamUsers.length > 0 && (
+                        <InvintationBlock
+                            styles={styles}
+                            teamInfo={teamInfo}
+                            handleSendInvite={handleSendInvite}
+                            handleInputChange={handleInputChange}
+                            inviteEmail={inviteEmail}
+                            searchTerm={searchTerm}
+                            filteredUsers={filteredUsers}
+                            handleUserClick={handleUserClick}
+                        />
+                    )}
+                </div>
             </div>
         </div>
     );
